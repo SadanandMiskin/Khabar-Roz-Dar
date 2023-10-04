3663b7222ee31b9b40b55fb53e3092a4697ec8af
SadanandMiskin <miskinsadanand@gmail.com>
2023-10-05 00:50:42 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const ejs = require('ejs')
+const axios = require('axios')
+require('dotenv').config()
+
+
+const app = express()
+app.set('view engine','ejs')
+app.use(express.static(__dirname + '/public'))
+
+
+const PORT = process.env.PORT
+app.get('/',async (req,res)=>{
+    try{
+        const response = await axios.get('https://real-gray-anemone-belt.cyclic.app/news')
+        const responseData = response.data
+        res.render('index' , {responseData})
+    }
+    catch(err){
+        console.log(err)
+    }
+    
+})
+
+app.listen(PORT || 3000 , ()=>{
+    console.log('Server listening')
+})
